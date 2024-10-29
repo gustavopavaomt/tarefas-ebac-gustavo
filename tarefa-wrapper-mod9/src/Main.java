1ccd4f4adbb2d11f4b5def65a9842b8b8024cf28
gustavo.pavao <suporte.gustavopavao@gmail.com>
2024-10-28 20:04:04 -0400

    Versionando o modulo 9 curso da ebac.

---
+++ tarefa-wrapper-mod9/src/Main.java
@@ -0,0 +1,11 @@
+class Main{
+
+    public static void main(String[] args) {
+
+        int valorPrimitivo = 35;
+        Integer valorWapper = valorPrimitivo;
+
+        System.out.println("Valor primitivo: "+valorPrimitivo);
+        System.out.println("Valor wrapper: "+valorWapper);
+    }
+}
